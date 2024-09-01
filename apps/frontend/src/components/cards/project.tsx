dbba0dfc061a606753d210060e5accf9cf60eed5
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-09-01 21:15:46 +0100

    feat(project - wip): card

--- apps/frontend/src/components/cards/project.tsx
+++ apps/frontend/src/components/cards/project.tsx
@@ -1,43 +1,60 @@
-import { For, Show } from 'solid-js';
+import { Image } from '@unpic/solid';
+import { type ComponentProps, For, Show, mergeProps, splitProps } from 'solid-js';
 import { urlFor } from '~/cms';
+import { Badge } from '~/components/ui';
 import type { ProjectCard as ProjectCardType } from '~/shared/types';
 import { cn } from '~/shared/utils';
+import { getBlurHashImage } from '~/shared/utils/images';
 
-function ProjectCard(props: ProjectCardType) {
+type Props = ProjectCardType & Pick<ComponentProps<'a'>, 'class'>;
+
+function ProjectCard(rawProps: Props) {
+  const props = mergeProps(rawProps, {
+    class: '',
+  });
+  const [projectProps, styleProps, otherProps] = splitProps(
+    props,
+    ['thumbnail', '_id', 'slug', 'headline', 'services', 'title'],
+    ['class'],
+  );
+
   return (
     <a
-      href={`/projetos/${props.slug}`}
-      class="group shadow transition duration-300 ease-in-out"
+      {...otherProps}
+      href={`/projetos/${projectProps.slug}`}
+      class={cn(['group shadow transition duration-300 ease-in-out', styleProps.class])}
     >
       <article
         class={cn(['relative isolate', 'overflow-hidden', 'h-[500px] rounded-lg'])}
       >
-        <img
-          src={urlFor(props.thumbnail).format('webp').maxWidth(1500).url()}
-          alt={props.title}
+        <Image
+          loading="lazy"
+          layout="fullWidth"
+          src={urlFor(projectProps.thumbnail)
+            .auto('format')
+            .quality(80)
+            .width(600)
+            .url()}
+          alt={projectProps.title}
+          background={getBlurHashImage(projectProps.thumbnail)}
           class="-z-10 absolute inset-0 size-full object-cover object-center transition-transform duration-300 ease-in-out group-hover:scale-105"
         />
 
-        <section class="flex size-full flex-col justify-end bg-gradient-to-b from-transparent to-80% to-gray-900/60 px-6 py-8 text-gray-50 transition-colors duration-300 ease-in-out group-hover:to-gray-900/80">
-          <h3 class="font-semibold text-white">{props.title}</h3>
+        <section class="flex size-full flex-col justify-end bg-gradient-to-b from-transparent to-foreground/60 px-6 py-8 text-gray-50 transition-colors duration-300 ease-in-out group-hover:to-foreground">
+          <h3 class="font-semibold text-white">{projectProps.title}</h3>
 
-          <Show when={props.headline}>
-            <p class="mt-1 mb-4 line-clamp-3 text-sm">{props.headline}</p>
+          <Show when={projectProps.headline}>
+            <p class="mt-1 mb-4 line-clamp-3 text-sm">{projectProps.headline}</p>
           </Show>
 
-          <Show when={props.services.length}>
+          <Show when={projectProps.services.length}>
             <ul class="flex flex-wrap items-center gap-2">
-              <For each={props.services}>
+              <For each={projectProps.services}>
                 {(service) => (
-                  <li class="inline-flex rounded-full border-white bg-gray-400/30 px-2 py-0.5 text-gray-50 text-sm">
-                    {/*<a*/}
-                    {/*  class={*/}
-                    {/*    'inline-flex rounded-full border-white bg-gray-400/30 px-2 py-0.5 font-semibold text-gray-50 text-sm'*/}
-                    {/*  }*/}
-                    {/*  href={`/servicos/${service.slug}`}*/}
-                    {/*>*/}
-                    {service.title}
-                    {/*</a>*/}
+                  <li>
+                    <Badge round={true} variant="secondary" class="pointer-events-none">
+                      {service.title}
+                    </Badge>
                   </li>
                 )}
               </For>
