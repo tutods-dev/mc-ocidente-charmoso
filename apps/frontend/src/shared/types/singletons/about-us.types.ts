2a943b2a6757d185c9edcdc17fb52eb7637f5b65
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2024-09-17 07:29:21 +0100

    feat(about us - wip): types

--- apps/frontend/src/shared/types/singletons/about-us.types.ts
+++ apps/frontend/src/shared/types/singletons/about-us.types.ts
@@ -1,3 +1,18 @@
+import type { ImageWithAlt, ImageWithAltAndCaption } from '~/shared/types';
 import type { AboutUs as AboutUsSchema } from '~/shared/types/sanity.types';
 
-type AboutUs = Omit<AboutUsSchema> & {};
+type AboutUs = Omit<AboutUsSchema, 'aboutUs' | 'factory' | 'process'> & {
+  aboutUs?: Omit<NonNullable<AboutUsSchema['aboutUs']>, 'thumbnail'> & {
+    thumbnail?: ImageWithAlt;
+  };
+
+  factory?: Omit<NonNullable<AboutUsSchema['factory']>, 'gallery'> & {
+    gallery?: ImageWithAltAndCaption[];
+  };
+
+  process?: Omit<NonNullable<AboutUsSchema['process']>, 'thumbnail'> & {
+    thumbnail?: ImageWithAlt;
+  };
+};
+
+export type { AboutUs };
--- apps/frontend/src/shared/types/singletons/index.ts
+++ apps/frontend/src/shared/types/singletons/index.ts
@@ -1,1 +1,2 @@
 export * from './settings.types';
+export * from './about-us.types';
