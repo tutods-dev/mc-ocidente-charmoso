28cfc426e87a0c49f1a1cebee1e51419bae8ce41
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2024-09-17 07:37:16 +0100

    feat(about us - wip): query

---
+++ apps/frontend/src/cms/queries/singletons/get-about-us.ts
@@ -0,0 +1,30 @@
+/**
+ * Query to retrieve the about us page.
+ */
+const getAboutUsQuery = `
+   *[_type == "about-us"] [0] {
+        "process": select(defined(process) => {
+            ...process,            
+            "thumbnail": select(defined(process.thumbnail) => {
+                ...process.thumbnail,
+                "asset": process.thumbnail.asset->
+           }),
+        }),
+        "aboutUs": select(defined(aboutUs) => {
+            ...aboutUs,            
+            "thumbnail": select(defined(aboutUs.thumbnail) => {
+                ...aboutUs.thumbnail,
+                "asset": aboutUs.thumbnail.asset->
+           }),
+        }),
+        "factory": select(defined(factory) => {
+            ...factory,            
+           "gallery": coalesce(factory.gallery[] {
+                ...,
+                "asset": asset->
+            }, [])
+        }) 
+   }
+`;
+
+export { getAboutUsQuery };
