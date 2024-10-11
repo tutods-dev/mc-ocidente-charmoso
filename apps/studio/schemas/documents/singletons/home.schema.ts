eecd157bf3d8bd9ef80efa71ee87aab30c4c8d4d
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-10-11 13:35:18 +0100

    feat(wip): home

--- apps/studio/schemas/documents/singletons/home.schema.ts
+++ apps/studio/schemas/documents/singletons/home.schema.ts
@@ -1,3 +1,4 @@
+import { isTextRequired } from '@/shared';
 import { defineField, defineType } from 'sanity';
 
 const homeSchema = defineType({
@@ -11,9 +12,33 @@ const homeSchema = defineType({
   },
   fields: [
     defineField({
-      name: 'test',
-      title: 'Test',
-      type: 'string',
+      name: 'projects',
+      title: 'Secção de Projetos',
+      type: 'object',
+      fields: [
+        defineField({
+          title: 'Título',
+          name: 'title',
+          type: 'string',
+          validation: (Rule) => isTextRequired(Rule, 'título'),
+        }),
+        defineField({
+          description:
+            'A breve descrição é especialmente utilizada para melhorar o SEO da página.',
+          name: 'headline',
+          rows: 3,
+          title: 'Breve descrição',
+          type: 'text',
+          validation: (Rule) => [
+            Rule.required().warning(
+              'A breve descrição não é obrigatória, porém poderá ajudar a melhorar o SEO da página!',
+            ),
+            Rule.max(160).warning(
+              'Atenção! Descrições longas podem ser cortadas pelos motores de pesquisa a quando da sua visualização.',
+            ),
+          ],
+        }),
+      ],
     }),
   ],
 });
