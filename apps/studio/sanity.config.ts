7398b75113c4ba037e5c57d80209c77e8f1a0c80
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-10-12 22:26:48 +0100

    feat: add media plugin

--- apps/studio/sanity.config.ts
+++ apps/studio/sanity.config.ts
@@ -2,6 +2,7 @@ import { CogIcon, EnvelopeIcon, HomeIcon, InfoOutlineIcon } from '@sanity/icons'
 import { ptPTLocale } from '@sanity/locale-pt-pt';
 import { visionTool } from '@sanity/vision';
 import { defineConfig } from 'sanity';
+import { media } from 'sanity-plugin-media';
 import { structureTool } from 'sanity/structure';
 import { schemaTypes } from './schemas';
 import { singletonTypes, singletonsActions } from './shared';
@@ -75,5 +76,6 @@ export default defineConfig({
       title: 'Português',
     }),
     visionTool(),
+    media(),
   ],
 });
