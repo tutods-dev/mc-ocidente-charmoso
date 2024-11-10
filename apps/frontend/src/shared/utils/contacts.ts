e10f1399ce58e7dea149b0d5c3f75c96a8e98fad
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-10 12:12:46 +0000

    chore: add +351 to phone contacts

--- apps/frontend/src/shared/utils/contacts.ts
+++ apps/frontend/src/shared/utils/contacts.ts
@@ -2,12 +2,12 @@
  * Callback function to get the beginning of a contact url.
  * @param type Indicates if it's a phone number or an email.
  */
-function getContactUrl(type: 'email' | 'phone') {
-  if (type === 'phone') {
-    return 'tel:';
-  }
+function getContactUrl(type: "email" | "phone") {
+	if (type === "phone") {
+		return "tel:+351";
+	}
 
-  return 'mailto:';
+	return "mailto:";
 }
 
 /**
@@ -15,31 +15,31 @@ function getContactUrl(type: 'email' | 'phone') {
  * @param phone Phone number
  */
 function getFormattedPhone(phone: string) {
-  const regex = /.{1,3}/g;
-  let formattedPhone = phone;
+	const regex = /.{1,3}/g;
+	let formattedPhone = phone;
 
-  if (phone.startsWith('+351')) {
-    formattedPhone = phone.replace('+351', '');
-  }
+	if (phone.startsWith("+351")) {
+		formattedPhone = phone.replace("+351", "");
+	}
 
-  return ['+351', formattedPhone.match(regex)].flat().join(' ');
+	return ["+351", formattedPhone.match(regex)].flat().join(" ");
 }
 
 /**
  * Callback function to retrieve the icon for each type of contact.
  * @param type Contact type
  */
-function getContactIcon(type: 'phone' | 'email') {
-  switch (type) {
-    case 'phone':
-      return 'ph ph-phone';
+function getContactIcon(type: "phone" | "email") {
+	switch (type) {
+		case "phone":
+			return "ph ph-phone";
 
-    case 'email':
-      return 'ph ph-at';
+		case "email":
+			return "ph ph-at";
 
-    default:
-      return '';
-  }
+		default:
+			return "";
+	}
 }
 
 export { getFormattedPhone, getContactIcon, getContactUrl };
