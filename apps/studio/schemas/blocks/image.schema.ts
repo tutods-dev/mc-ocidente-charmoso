b6b7337e4461a7d41f1b5a72897fa68d342e670b
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-14 07:14:07 +0000

    chore: titles

--- apps/studio/schemas/blocks/image.schema.ts
+++ apps/studio/schemas/blocks/image.schema.ts
@@ -1,56 +1,58 @@
-import { defineField, defineType } from 'sanity';
-import { isFileRequired, isTextRequired } from '../../shared/validations';
+import { defineField, defineType } from "sanity";
+import { isFileRequired, isTextRequired } from "../../shared/validations";
 
 const imageWithAltSchema = defineType({
-  type: 'image',
-  name: 'imageWithAlt',
-  options: {
-    hotspot: true,
-    metadata: ['blurhash', 'lqip', 'exif'],
-  },
-  fields: [
-    defineField({
-      hidden: ({ parent }) => !parent?.asset,
-      name: 'alt',
-      title: 'Texto alternativo',
-      type: 'string',
-      description:
-        'O texto alternativo serve como legenda oculta da imagem, é utilizada por razões de SEO.',
-      validation: (Rule) => isTextRequired(Rule, 'texto alternativo'),
-    }),
-  ],
-  validation: (Rule) => isFileRequired(Rule, 'imagem'),
+	type: "image",
+	name: "imageWithAlt",
+	title: "Imagem com texto alternativo",
+	options: {
+		hotspot: true,
+		metadata: ["blurhash", "lqip", "exif"],
+	},
+	fields: [
+		defineField({
+			hidden: ({ parent }) => !parent?.asset,
+			name: "alt",
+			title: "Texto alternativo",
+			type: "string",
+			description:
+				"O texto alternativo serve como legenda oculta da imagem, é utilizada por razões de SEO.",
+			validation: (Rule) => isTextRequired(Rule, "texto alternativo"),
+		}),
+	],
+	validation: (Rule) => isFileRequired(Rule, "imagem"),
 });
 
 const imageWithAltAndCaptionSchema = defineType({
-  type: 'image',
-  name: 'imageWithAltAndCaption',
-  options: {
-    hotspot: true,
-    metadata: ['blurhash', 'lqip', 'exif'],
-  },
-  fields: [
-    defineField({
-      hidden: ({ parent }) => !parent?.asset,
-      name: 'alt',
-      title: 'Texto alternativo',
-      type: 'string',
-      description:
-        'O texto alternativo serve como legenda oculta da imagem, é utilizada por razões de SEO.',
-      validation: (Rule) => isTextRequired(Rule, 'texto alternativo'),
-    }),
-    defineField({
-      hidden: ({ parent }) => !parent?.asset,
-      name: 'caption',
-      title: 'Caption',
-      type: 'string',
-      validation: (Rule) =>
-        Rule.required().warning(
-          'A legenda da imagem não é obrigatória, porém pode ajudar a melhorar o SEO da página.',
-        ),
-    }),
-  ],
-  validation: (Rule) => isFileRequired(Rule, 'imagem'),
+	type: "image",
+	name: "imageWithAltAndCaption",
+	title: "Imagem com texto alternativo e legenda",
+	options: {
+		hotspot: true,
+		metadata: ["blurhash", "lqip", "exif"],
+	},
+	fields: [
+		defineField({
+			hidden: ({ parent }) => !parent?.asset,
+			name: "alt",
+			title: "Texto alternativo",
+			type: "string",
+			description:
+				"O texto alternativo serve como legenda oculta da imagem, é utilizada por razões de SEO.",
+			validation: (Rule) => isTextRequired(Rule, "texto alternativo"),
+		}),
+		defineField({
+			hidden: ({ parent }) => !parent?.asset,
+			name: "caption",
+			title: "Caption",
+			type: "string",
+			validation: (Rule) =>
+				Rule.required().warning(
+					"A legenda da imagem não é obrigatória, porém pode ajudar a melhorar o SEO da página.",
+				),
+		}),
+	],
+	validation: (Rule) => isFileRequired(Rule, "imagem"),
 });
 
 export { imageWithAltSchema, imageWithAltAndCaptionSchema };
--- apps/studio/schemas/blocks/video.schema.ts
+++ apps/studio/schemas/blocks/video.schema.ts
@@ -1,26 +1,27 @@
-import { defineField, defineType } from 'sanity';
+import { defineField, defineType } from "sanity";
 
 const videoUrlWithCoverSchema = defineType({
-  name: 'videoUrlWithCover',
-  type: 'object',
-  fields: [
-    defineField({
-      title: 'Link do vídeo',
-      name: 'url',
-      type: 'url',
-      description:
-        'Coloque o link do vídeo extraído do ImageKit, Cloudinary ou outra plataform. Nota: vídeos do YouTube não irão funcionar.',
-      //   TODO: validations
-    }),
-    defineField({
-      name: 'cover',
-      title: 'Imagem alternativa',
-      hidden: ({ parent }) => !parent || !parent.url,
-      type: 'imageWithAlt',
-      description:
-        'A imagem alternativa será apresentada em quanto o vídeo é carregado.',
-    }),
-  ],
+	name: "videoUrlWithCover",
+	title: "Vídeo",
+	type: "object",
+	fields: [
+		defineField({
+			title: "Link do vídeo",
+			name: "url",
+			type: "url",
+			description:
+				"Coloque o link do vídeo extraído do ImageKit, Cloudinary ou outra plataform. Nota: vídeos do YouTube não irão funcionar.",
+			//   TODO: validations
+		}),
+		defineField({
+			name: "cover",
+			title: "Imagem alternativa",
+			hidden: ({ parent }) => !parent || !parent.url,
+			type: "imageWithAlt",
+			description:
+				"A imagem alternativa será apresentada em quanto o vídeo é carregado.",
+		}),
+	],
 });
 
 export { videoUrlWithCoverSchema };
