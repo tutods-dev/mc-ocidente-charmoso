f4c0f0b2af7d6b7a2eaa1d3ae5347bf911876a0c
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-19 07:58:36 +0000

    fix: language

--- apps/frontend/src/entry-server.tsx
+++ apps/frontend/src/entry-server.tsx
@@ -4,7 +4,7 @@ import { StartServer, createHandler } from '@solidjs/start/server';
 export default createHandler(() => (
   <StartServer
     document={({ assets, children, scripts }) => (
-      <html lang="en">
+      <html lang="pt">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
