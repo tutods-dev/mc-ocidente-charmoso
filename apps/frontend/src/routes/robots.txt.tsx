66fc82562b2c2b2abfaac36fafa4bf6410192b44
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-21 19:56:46 +0000

    fix: robots

--- apps/frontend/src/routes/robots.txt.tsx
+++ apps/frontend/src/routes/robots.txt.tsx
@@ -3,9 +3,18 @@ import type { APIEvent } from '@solidjs/start/server';
 export function GET({ request }: APIEvent) {
   const baseUrl = new URL(request.url).origin;
 
-  const robotsTxt = `User-agent: *\nAllow: /\nSitemap: ${baseUrl}/sitemap.xml`;
+  const txtParts = [
+    'User-agent: *',
+    'Allow: /',
+    `Sitemap: ${baseUrl}/sitemap.xml`,
+    '# Certain social media sites are whitelisted to allow crawlers to access page markup when links to /images are shared.',
+    'User-agent: Twitterbot',
+    'Allow: /',
+    'User-agent: facebookexternalhit',
+    'Allow: /',
+  ];
 
-  return new Response(robotsTxt.trim(), {
+  return new Response(txtParts.join('\n').trim(), {
     headers: {
       'Content-Type': 'text/plain',
     },
