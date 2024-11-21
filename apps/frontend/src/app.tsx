771400420970d596479006c231458c8f869c9d00
Daniel Sousa <jdaniel.asousa@gmail.com>
2024-11-21 20:29:12 +0000

    fix: seo

--- apps/frontend/src/app.tsx
+++ apps/frontend/src/app.tsx
@@ -1,4 +1,4 @@
-import { MetaProvider } from '@solidjs/meta';
+import { Link, Meta, MetaProvider } from '@solidjs/meta';
 import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
@@ -30,6 +30,22 @@ function App() {
       root={(props) => (
         <Suspense fallback={<LoadingScreen />}>
           <MetaProvider>
+            {/* Default meta tags */}
+            <Meta charset="UTF-8" />
+            <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <Meta http-equiv="content-language" content="Portuguese" />
+            <Meta name="language" content="Portuguese" />
+
+            {/* Default social tags */}
+            <Meta property="og:locale" content="Portuguese" />
+            <Meta property="og:type" content="website" />
+
+            {/* Favicon */}
+            <Link rel="icon" href="/logo.svg" type="image/svg+xml" />
+
+            {/* Robots */}
+            <Meta name="robots" content="index, follow" />
+
             {/* <DefaultSeo /> */}
             <Header mode="solid" />
 
--- apps/frontend/src/components/seo/page.tsx
+++ apps/frontend/src/components/seo/page.tsx
@@ -1,4 +1,4 @@
-import { Link, Meta, Title } from '@solidjs/meta';
+import { Meta, Title } from '@solidjs/meta';
 import { createAsync } from '@solidjs/router';
 import { type JSXElement, Match, Show, Switch, mergeProps } from 'solid-js';
 import { getSeoSettings } from '~/cms/services/singletons/settings';
@@ -62,28 +62,15 @@ function PageSeo(rawProps: Props) {
         </Match>
       </Switch>
 
-      {/* Default meta tags */}
-      <Meta charset="UTF-8" />
-      <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <Meta http-equiv="content-language" content="Portuguese" />
-      <Meta name="language" content="Portuguese" />
-
-      {/* Default social tags */}
-      <Meta property="og:locale" content="Portuguese" />
-      <Meta property="og:type" content="website" />
-
-      {/* Favicon */}
-      <Link rel="icon" href="/logo.svg" type="image/svg+xml" />
-
-      {/* Robots */}
-      <Meta name="robots" content="index, follow" />
-
       <Meta name="description" content={props.description ?? seoSettings()?.description} />
       <Meta name="keywords" content={[seoSettings()?.keywords, props.keywords].flat().join(', ')} />
 
       {/* Social tags */}
       <Meta property="og:site_name" content={seoSettings()?.title} />
-      <Meta property="og:title" content={`${props.title} | ${seoSettings()?.title}`} />
+      <Meta
+        property="og:title"
+        content={props.title ? `${props.title} | ${seoSettings()?.title}` : seoSettings()?.title}
+      />
       <Meta property="og:image" content={props.image} />
       <Meta property="og:image:alt" content={props.title ?? seoSettings()?.title} />
       <Meta property="og:description" content={props.description} />
